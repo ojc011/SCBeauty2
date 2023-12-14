540205516e3eccd36d1297663034edb3e76fb45a
Oliver Cronk <ojc011@yahoo.com>
2023-12-14 09:32:24 -0500

    Updated pricing

--- src/components/pricing/PricingTable.js
+++ src/components/pricing/PricingTable.js
@@ -5,6 +5,19 @@ import PricingPic from '../../assets/images/PricingPic.jpeg';
 const BridalPricing = () => {
   return (
     <PricingSlot title="Bridal">
+            <PricingDetail>
+        <b>Bridal Services:</b>
+        <h3>
+          <b>Day of Bridal Hair: $125 (Add $45 for clip in extension applications)</b>
+        </h3>
+        <h3>
+          <b>Day of Bridal Makeup: $145 (Includes individual lashes, and airbrush)</b>
+        </h3>
+        <h3>
+          <b>Bridal Trial Run: $215 (Includes travel within Austin) </b>
+        </h3>
+      </PricingDetail>
+      <br />
       <PricingDetail>
         <b>Natural Beauty $450</b>
         <h1>Day of Hair + Airbrush Makeup + Trial</h1>
@@ -30,22 +43,25 @@ const BridalPricing = () => {
       <br />
       <PricingDetail>
         <h3>
-          <b>Groom Styling: $40</b>
+          <b>Day of Bridesmaid/Family Hair: $95</b>
         </h3>
         <h3>
-          <b>Day of Bridesmaid/Family Hair: $90</b>
+          <b>Day of Bridesmaid/Family Makeup: $105 </b>
         </h3>
         <h3>
-          <b>Day of Bridesmaid/Family Makeup: $100 </b>
-        </h3>
-        <h3>
-          <b>Blowout/Style: $75</b>
+          <b>Blowout/Style: $80</b>
         </h3>
         <h3>
           <b>Flower Girl Hairstyle: $50</b>
         </h3>
         <h3>
           <b>Flower Girl Makeup Touch: $45</b>
+        </h3>
+        <h3>
+          <b>Groom Styling: $40</b>
+        </h3>
+        <h3>
+          <b>Makeup Lesson - (2 Hours): $150</b>
         </h3>
         <h3>
           <b>Photoshoot/Event Hourly rate: $110 per hour</b>
@@ -62,9 +78,9 @@ const BridalPricing = () => {
       </PricingDetail>
       <br />
       <PricingDetail>
-        <h1>Travel Fee within Austin: $20.00 Per Artist</h1>
+        <h1>Travel Fee within Austin: $35.00 Per Artist</h1>
         <h1>
-          Travel Fee Outside of Austin: $20.00 plus $0.75 per mile (to and
+          Travel Fee Outside of Austin: $35.00 plus $1.00 per mile (to and
           from) per artist
         </h1>
       </PricingDetail>
